b1a57fd04621a632f2cdbfabd57652ad2307144f
Jeroen Janssen <toproper@icloud.com>
2020-04-16 11:10:53 +0200

    Changed scoping of title block

--- build/umc-section.entry.js
+++ build/umc-section.entry.js
@@ -1,11 +1,11 @@
-import { r as registerInstance, h, H as Host } from './core-029ba0c7.js';
+import { r as registerInstance, h } from './core-029ba0c7.js';
 
 const Section = class {
     constructor(hostRef) {
         registerInstance(this, hostRef);
     }
     render() {
-        return (h(Host, null, h("slot", null)));
+        return (h("div", null, h("slot", null)));
     }
     static get style() { return ":host {\n  -webkit-box-sizing: border-box;\n  box-sizing: border-box;\n  background: pink;\n  display: -ms-flexbox;\n  display: flex;\n  -ms-flex-align: center;\n  align-items: center;\n  -ms-flex-direction: column;\n  flex-direction: column;\n  --background-color--default: $white;\n  background-color: var(--background-color, var(--background-color--default));\n  width: 100%;\n}"; }
 };
--- build/umc-title-block.entry.js
+++ build/umc-title-block.entry.js
@@ -6,7 +6,9 @@ const TitleBlock = class {
         this.size = 'regular';
     }
     render() {
-        return (h("h2", { class: this.size }, this.title));
+        return (h("slot", null)
+        // <h2 class={this.size}>{this.title}</h2>
+        );
     }
     static get style() { return "h2 {\n  font-family: \"Museo Sans Rounded\", sans-serif;\n  font-weight: 300;\n  font-size: 3rem;\n  margin: 0;\n  line-height: 3.5rem;\n  color: #131516;\n}\n\nh2.large {\n  font-size: 3rem;\n}\n\n.regular {\n  font-size: 2rem;\n}"; }
 };
