6327fc9f03c01ccbe31296a5581ab0a1ef1786d2
Jeroen Janssen <toproper@icloud.com>
2020-04-27 22:18:49 +0200

    New release v-0.0.1

--- build/p-pq5d0nid.system.entry.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-7a3e14c1.system.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.r}],execute:function(){var i=t("umc_link_list_item",function(){function t(t){e(this,t);this.active=false}return t}())}}}));
